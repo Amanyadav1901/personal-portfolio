3047461a4bcd7106755ea9b658e04bab6e57d055
Amanyadav1901 <amanyadav_co20b2_51@dtu.ac.in>
2025-03-14 07:31:44 +0530

    adding profile pic

--- components/About.jsx
+++ components/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import AboutImg from "../public/assets/about.jpg";
+import profileImg from "../public/assets/profilepic.jpeg";
 
 const About = () => {
   return (
@@ -51,7 +52,7 @@ const About = () => {
           </Link>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-          <Image src={AboutImg} className="rounded-xl" alt="/" />
+          <Image src={profileImg} className="rounded-xl" alt="/" />
         </div>
       </div>
     </div>
